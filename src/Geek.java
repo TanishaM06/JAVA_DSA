2e9a1067a5b33ebf5feecc8ca8cd0fa3bdf6e853
TanishaM06 <manetanisha06@gmail.com>
2025-07-30 23:14:23 +0530

    Initial commit of cleaned Java DSA project

---
+++ src/Geek.java
@@ -0,0 +1,10 @@
+public class Geek {
+    public static void main(String [] args) {
+		String message = greek();
+		System.out.print(message);
+		}
+	static String greek() {
+		String greeking ="Hello I am Plant I LOVE YOU";
+		return greeking;
+	}
+}
